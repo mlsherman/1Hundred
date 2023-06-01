a06b5cb5fb5982d443c83ddc528e6e3326753e1b
mlsherman <advancedautosec@yahoo.com>
2023-06-01 13:55:46 -0400

    finishing Js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,67 @@
-// Call Hello World 
-function HelloWorld() {
-    alert("Hello World")
+
+// get the values from the interface
+// starts or controller function
+function getValues() {
+    // get values from page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    
+    // we need to validate our input
+    // parse into integers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    if(Number.isInteger(startValue) && Number.isInteger(endValue)){
+        // we call generateNumbers
+        let numbers = generateNumbers(startValue, endValue)
+         // we call displayNumbers
+        displayNumbers(numbers);
+
+    } else {
+        alert("You must enter integers")
+    }
+    
+    
+}
+
+// generate numbers from startvalue to the endValue
+// logic function(s)
+function generateNumbers(sValue, eValue) {
+
+    let numbers = []
+
+    // we want to get all numbers from start to end
+    for(let index = sValue; index <= eValue; index++) {
+
+        // this will execute in a loop until index = eValue
+        numbers.push(index);
+    }
+
+    return numbers 
+}
+
+// display the even numbers in bold type
+// display or view function
+function displayNumbers(numbers) {
+
+    let templateRows = "";
+
+    for (let index = 0; index < numbers.length; index++) {
+        
+        let className = "even";
+        let number = numbers[index];
+
+        if(number % 2 == 0){
+            className = "even"
+        }
+        else{
+            className = "odd"
+
+        }
+
+        templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+    }
+
+    document.getElementById("results").innerHTML = templateRows
+
 }
